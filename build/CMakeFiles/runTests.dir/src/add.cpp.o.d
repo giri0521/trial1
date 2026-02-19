707913f71132aa6b081300b16449f01ad0869188
giri0521 <125133722+giri0521@users.noreply.github.com>
2026-02-19 09:51:55 +0000

    Changes Made

---
+++ build/CMakeFiles/runTests.dir/src/add.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/runTests.dir/src/add.cpp.o: /workspaces/trial1/src/add.cpp \
+ /usr/include/stdc-predef.h /workspaces/trial1/src/add.h
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/trial1
+# Build directory: /workspaces/trial1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyTests "/workspaces/trial1/build/runTests")
+set_tests_properties(MyTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/trial1/CMakeLists.txt;26;add_test;/workspaces/trial1/CMakeLists.txt;0;")

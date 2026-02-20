3f62f388abc08f02b73c1e42aa01d0efdca6355d
giri0521 <125133722+giri0521@users.noreply.github.com>
2026-02-20 07:36:36 +0000

    Changes made in test file

---
+++ build/CMakeFiles/main.dir/src/add.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/src/add.cpp.o: /workspaces/trial1/src/add.cpp \
+ /usr/include/stdc-predef.h /workspaces/trial1/src/add.h
--- test/test_add.cpp
+++ test/test_add.cpp
@@ -9,10 +9,10 @@ TEST(Addtest, PositiveNumbers){
     EXPECT_EQ(add(1,2), 3);
 }
 
-TEST(Addtest, DefaultParameters){
-    EXPECT_EQ(add(), 0);
-}
-
 TEST(Addtest, MixedNumbers){
     EXPECT_EQ(add(-5,4), -1);
+}
+
+TEST(AddTest, ZeroNumbers){
+    EXPECT_EQ(add(), 0);
 }
